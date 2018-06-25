533b63a0081a7a9d05cccc939112156821c45327
mattwecht <mattwecht@gmail.com>
2018-06-25 16:53:05 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,26 +7,26 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+def first_student_by_index 
+  return STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  return STUDENT_NAMES[3] 
 end
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  return STUDENT_NAMES[5]
 end
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  STUDENT_NAMES.first
 end
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  a = [STUDENT_NAMES[0],STUDENT_NAMES[1],STUDENT_NAMES[2]]
 end
